86520e9cf13cf236fe6de219cba0ef0d158af861
Wender Mrn <wender.mrn@gmail.com>
2017-08-17 00:24:06 -0300

    add icone na listagem de veículos

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :vehicles
   resources :maintenances
   
-	root 'home#index'
+	root 'vehicles#index'
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
