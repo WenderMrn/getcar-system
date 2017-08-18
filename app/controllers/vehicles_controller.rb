c43464caec3092db93d05bcb2b0f6e11ab1b2eae
Vicente Correia <vicente.correia@academico.ifpb.edu.br>
2017-08-17 23:10:41 -0300

    [add]
    adicionando paginação aos veículos

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,6 +1,6 @@
 class VehiclesController < ApplicationController
 	def index
-		@vehicles = Vehicle.all
+		@vehicles = Vehicle.page(params[:page])
 	end
 
 	def show
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle < ApplicationRecord
 	has_many :maintenances
 	has_many :rents
+	paginates_per 6
 end
