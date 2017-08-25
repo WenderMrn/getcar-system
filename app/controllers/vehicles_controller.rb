a256063175a977261b4d5648e7967098ff76809b
Wender Mrn <wender.mrn@gmail.com>
2017-08-24 21:27:44 -0300

    adicionando action para deletar veículo

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -14,10 +14,23 @@ class VehiclesController < ApplicationController
 	def edit
 	end
 
+	def destroy
+		@vehicle = Vehicle.find(params[:id])
+		@vehicle.destroy
+
+		if @vehicle.destroyed?
+			flash[:notice] = "O veículo #{@vehicle.model.upcase} foi deletado com sucesso."
+			redirect_to vehicles_path
+		else
+			flash.now[:alert] = "O veículo #{@vehicle.model.upcase} não pode ser deletado."
+			render vehicles_path
+		end
+	end
+
 	def create
 		@vehicle = Vehicle.new(vehicle_param)
-
     if @vehicle.save
+    	flash[:notice] = "O veículo #{@vehicle.model.upcase} foi adicionado com sucesso."
       redirect_to @vehicle
     else
       render 'new'
